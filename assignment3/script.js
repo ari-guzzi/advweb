cdbb35c4417a51e480ec44babaf9ee0e0dcab5ff
Ari Guzzi <argu4451@colorado.edu>
2024-09-30 13:21:02 -0600

    added vite and lodash

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
   function fetchBear() {
             const width = Math.floor(Math.random() * 301) + 600;  
             const height = Math.floor(Math.random() * 301) + 600; 
