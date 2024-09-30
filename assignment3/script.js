400f6edfa9c4c3962b8812be6f702d3084636845
Ari Guzzi <argu4451@colorado.edu>
2024-09-29 20:17:29 -0600

    assignment 3

---
+++ assignment3/script.js
@@ -0,0 +1,16 @@
+  function fetchBear() {
+            const width = Math.floor(Math.random() * 301) + 600;  
+            const height = Math.floor(Math.random() * 301) + 600; 
+            //random widths and heights between 300 and 600
+
+            const imageUrl = `https://placebear.com/${width}/${height}`;
+
+            const img = new Image(width, height);
+            img.src = imageUrl;
+            img.alt = 'A random bear image';
+
+       
+            const container = document.getElementById('bearContainer');
+            container.innerHTML = ''; //remove previous image
+            container.appendChild(img);
+        }
